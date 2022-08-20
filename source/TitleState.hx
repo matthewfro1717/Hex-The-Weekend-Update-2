9cb73a4a9cc087d0b90fa7ed74cdf7d899ffb926
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-08-20 14:08:42 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -297,13 +297,13 @@ class TitleState extends MusicBeatState
 					returnedData[0] = data.substring(0, data.indexOf(';'));
 					returnedData[1] = data.substring(data.indexOf('-'), data.length);
 
-					switchState(new PlayState());
+					switchState(new HexStoryMenu());
 				}
 
 				http.onError = function(error)
 				{
 					trace('error: $error');
-					switchState(new PlayState());
+					switchState(new HexStoryMenu());
 					clean();
 				}
 
